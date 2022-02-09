5e5f4a4aea014e0aad2edc48f1414a2a266b0532
Nicolas Moreau <nicolas.moreau93@orange.fr>
2022-02-09 12:14:33 +0100

    initialisation composant Avis

---
+++ src/app/app.componentAvis.ts
@@ -0,0 +1,12 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-root',
+  templateUrl: './app.component.html',
+  styleUrls: ['./app.component.scss'],
+  template : '<h1>Avis</h1>'
+})
+
+export class Avis {
+  
+}
