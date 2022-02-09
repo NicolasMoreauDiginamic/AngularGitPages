c701c5db4b0e548e15265426a657f74dc730a2f0
Nicolas Moreau <nicolas.moreau93@orange.fr>
2022-02-09 13:56:36 +0100

    correction componant avis 4

--- src/app/Avis/app.componentAvis.ts
+++ src/app/Avis/app.componentAvis.ts
@@ -2,11 +2,8 @@ import { Component } from '@angular/core';
 
 @Component({
   selector: 'avis',
-  template : `
-    <h1>Avis</h1>
-    <button (click)="aime()">j'aime</button>
-    <button (click)="deteste()>je deteste</button>
-  `
+  template : '<h1>Avis</h1><button (click)="aime()">J`aime</button><button (click)="deteste()>Je deteste</button>'
+    
 })
 
 export class Avis {
