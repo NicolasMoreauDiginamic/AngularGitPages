e5ed9a5643ef143f8827ec1d4fb5a2f5a0a96ecd
Nicolas Moreau <nicolas.moreau93@orange.fr>
2022-02-09 12:37:42 +0100

    component avis

--- src/app/app.componentAvis.ts
+++ src/app/app.componentAvis.ts
@@ -2,9 +2,7 @@ import { Component } from '@angular/core';
 
 @Component({
   selector: 'avis',
-  templateUrl: './app.component.html',
-  styleUrls: ['./app.component.scss'],
-  template : '<h1>Avis</h1>'
+  template : `<h1>Avis</h1>`
 })
 
 export class Avis {
