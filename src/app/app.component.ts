21014fe773d3bb5a6b5aab145773875e4ba57a7f
Nicolas Moreau <nicolas.moreau93@orange.fr>
2022-02-09 15:34:11 +0100

    correction componant collegue 3

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,4 +1,5 @@
 import { Component } from '@angular/core';
+import { Collegue } from './models/collegue';
 
 @Component({
   selector: 'app-root',
@@ -8,4 +9,9 @@ import { Component } from '@angular/core';
 
 export class AppComponent {
   title = 'top-collegues';
+  unCollegue : Collegue = {
+    photoURL : '',
+    pseudo: 'test',
+    score: '+500'
+  }
 }
--- src/app/models/collegue.ts
+++ src/app/models/collegue.ts
@@ -1,6 +1,6 @@
 export interface Collegue {
     pseudo : string;
-    score : BigInteger;
+    score : string;
     photoURL : string;
 
 }
